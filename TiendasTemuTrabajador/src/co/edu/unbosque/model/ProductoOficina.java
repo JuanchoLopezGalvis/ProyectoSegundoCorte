9581543da5b44756a636c8c26ae8dc071cb93264
juanbuga <jmbuitragog@unbosque.edu.co>
2025-03-14 19:52:54 -0500

    Update ProductoOficina.java
    jejeje

--- TiendasTemuTrabajador/src/co/edu/unbosque/model/ProductoOficina.java
+++ TiendasTemuTrabajador/src/co/edu/unbosque/model/ProductoOficina.java
@@ -1,13 +1,14 @@
 package co.edu.unbosque.model;
 
 import java.awt.Image;
+import java.io.Serializable;
 
-public class ProductoOficina extends Producto{
-
+public class ProductoOficina extends Producto implements Serializable{
+	
 	/**
 	 * El uso del producto en la oficina
 	 */	
-	private String uso;
+	private String nivelRuido;
 	/**
 	 * Constructor vacio de la clase ProductoOficina
 	 */
@@ -18,37 +19,35 @@ public class ProductoOficina extends Producto{
 	 * @param uso es el uso del producto en la oficina
 	 */
 
-	public ProductoOficina(String uso) {
+	public ProductoOficina(String nivelRuido) {
 		super();
-		this.uso = uso;
+		this.nivelRuido = nivelRuido;
 	}
 	/**
 	 * Constructor de la clase ProductoOficina que recibe los atributos de la clase padre
 	 * @param nombre es el nombre del producto
 	 * @param precio es el precio del producto
 	 * @param cantidad es la cantidad de productos
-	 * @param tipo es el tipo de producto
 	 * @param marca es la marca del producto
 	 * @param imagen es la imagen del producto
-	 * @param uso es el uso del producto en la oficina
+	 * @param nivelRuido es la cantidad de ruido que genera
 	 */
-	public ProductoOficina(String nombre, double precio, int cantidad, String tipo, String marca, Image imagen,
-			String uso) {
-		super(nombre, precio, cantidad, tipo, marca, imagen);
-		this.uso = uso;
+	public ProductoOficina(String nombre, double precio, int cantidad, String marca, Image imagen,
+			String nivelRuido) {
+		super(nombre, precio, cantidad, marca, imagen);
+		this.nivelRuido = nivelRuido;
 	}
 	/**
 	 * Constructor de la clase ProductoOficina desde la clase padre Producto
 	 * @param nombre es el nombre del producto
 	 * @param precio es el precio del producto
 	 * @param cantidad es la cantidad de productos
-	 * @param tipo es el tipo de producto
 	 * @param marca es la marca del producto
 	 * @param imagen es la imagen del producto
 	 */
 
-	public ProductoOficina(String nombre, double precio, int cantidad, String tipo, String marca, Image imagen) {
-		super(nombre, precio, cantidad, tipo, marca, imagen);
+	public ProductoOficina(String nombre, double precio, int cantidad, String marca, Image imagen) {
+		super(nombre, precio, cantidad,marca, imagen);
 		// TODO Auto-generated constructor stub
 	}
 
@@ -56,17 +55,22 @@ public class ProductoOficina extends Producto{
 	 * Metodo que retorna el uso del producto en la oficina
 	 * @return uso
 	 */
-	public String getUso() {
-		return uso;
+	public String getNivelRuido() {
+		return nivelRuido;
 	}
 
 	/**
 	 * Metodo que modifica el uso del producto en la oficina
 	 * @param uso
 	 */
-	public void setUso(String uso) {
-		this.uso = uso;
+	public void setNivelRuido(String nivelRuido) {
+		this.nivelRuido = nivelRuido;
+	}
+	@Override
+	public String toString() {
+		return super.toString() + " Oficina [nivel de Ruido=" + nivelRuido + "]";
 	}
 	
 	
+
 }
