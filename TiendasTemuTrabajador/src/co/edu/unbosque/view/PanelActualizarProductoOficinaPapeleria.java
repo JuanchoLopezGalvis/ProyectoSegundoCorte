05dab384fbbbe140b501a71955600e69a57b4688
Juan Diego Lopez Galvis <jdilopezg@unbosque.edu.co>
2025-03-19 00:05:17 -0500

    Update PanelActualizarProductoOficinaPapeleria.java

--- TiendasTemuTrabajador/src/co/edu/unbosque/view/PanelActualizarProductoOficinaPapeleria.java
+++ TiendasTemuTrabajador/src/co/edu/unbosque/view/PanelActualizarProductoOficinaPapeleria.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package co.edu.unbosque.view;
 
 import javax.swing.JPanel;
@@ -6,12 +6,4 @@ import javax.swing.JPanel;
 public class PanelActualizarProductoOficinaPapeleria extends JPanel{
 
 }
-=======
-package co.edu.unbosque.view;
 
-import javax.swing.JPanel;
-
-public class PanelActualizarProductoOficinaPapeleria extends JPanel{
-
-}
->>>>>>> branch 'master' of git@github.com:JuanchoLopezGalvis/ProyectoSegundoCorte.git
