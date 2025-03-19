e607ac505b6f405bfde4b9c639d5b03864b54b92
Juan Diego Lopez Galvis <jdilopezg@unbosque.edu.co>
2025-03-19 00:09:59 -0500

    Update PanelPrueb.java

--- TiendasTemuTrabajador/src/co/edu/unbosque/view/PanelPrueb.java
+++ TiendasTemuTrabajador/src/co/edu/unbosque/view/PanelPrueb.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package co.edu.unbosque.view;
 
 import java.awt.GridLayout;
@@ -25,31 +25,4 @@ public class PanelPrueb extends JPanel{
 	}
 
 }
-=======
-package co.edu.unbosque.view;
 
-import java.awt.GridLayout;
-
-import javax.swing.JFileChooser;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.filechooser.FileNameExtensionFilter;
-
-public class PanelPrueb extends JPanel{
-	
-	private JFileChooser imagen;
-	private FileNameExtensionFilter filtro;
-	
-	public PanelPrueb() {
-		setSize(1250, 650);
-		setBorder(new EmptyBorder(10, 30, 30, 80));
-		setLayout(new GridLayout(1,1,1,1));
-		imagen = new JFileChooser();
-		filtro = new FileNameExtensionFilter("PNG", "png");
-		imagen.setFileFilter(filtro);
-		imagen.setAcceptAllFileFilterUsed(false);
-		add(imagen);
-	}
-
-}
->>>>>>> branch 'master' of git@github.com:JuanchoLopezGalvis/ProyectoSegundoCorte.git
