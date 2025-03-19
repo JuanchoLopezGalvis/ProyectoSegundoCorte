edebccea2d8727bdd707a65e21c8cc3dde012928
Juan Diego Lopez Galvis <jdilopezg@unbosque.edu.co>
2025-03-19 00:08:46 -0500

    Update PanelListarProductoHogarCocina.java

--- TiendasTemuTrabajador/src/co/edu/unbosque/view/PanelListarProductoHogarCocina.java
+++ TiendasTemuTrabajador/src/co/edu/unbosque/view/PanelListarProductoHogarCocina.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package co.edu.unbosque.view;
 
 import javax.swing.JPanel;
@@ -6,12 +6,3 @@ import javax.swing.JPanel;
 public class PanelListarProductoHogarCocina extends JPanel{
 
 }
-=======
-package co.edu.unbosque.view;
-
-import javax.swing.JPanel;
-
-public class PanelListarProductoHogarCocina extends JPanel{
-
-}
->>>>>>> branch 'master' of git@github.com:JuanchoLopezGalvis/ProyectoSegundoCorte.git
