f7062a9d2d703b584904ed2937c3e64992acf92e
Gomsss16 <jugovi1609@gmail.com>
2025-03-26 11:53:47 -0500

    Update NumberInStringException.java

--- TiendasTemuTrabajador/src/co/edu/unbosque/util/exception/NumberInStringException.java
+++ TiendasTemuTrabajador/src/co/edu/unbosque/util/exception/NumberInStringException.java
@@ -1,1 +1,9 @@
+package co.edu.unbosque.util.exception;
 
+public class NumberInStringException extends Exception {
+
+	public NumberInStringException() {
+
+	super ("Aca solo van letras, intente denuevo mas tarde");
+	}
+}
