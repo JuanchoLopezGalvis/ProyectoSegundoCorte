6eb7f4fd434f7a57cad52d0b37257078cf1a742c
Gomsss16 <jugovi1609@gmail.com>
2025-03-26 11:54:45 -0500

    Add files via upload

---
+++ TiendasTemuTrabajador/src/co/edu/unbosque/controller/ExceptionChecker.java
@@ -0,0 +1,33 @@
+package co.edu.unbosque.controller;
+
+import co.edu.unbosque.util.exception.NegativeNumberException;
+import co.edu.unbosque.util.exception.NumberInStringException;
+import co.edu.unbosque.util.exception.NumberOutOfBoundException;
+
+public class ExceptionChecker {
+
+	public static void checkNegativeNumber(int number) throws NegativeNumberException {
+		if (number < 0) {
+			throw new NegativeNumberException();
+		}
+
+	}
+
+	public static void checkNumberInString(String text) throws NumberInStringException {
+		if (text.matches(".*\\d.*")) {
+			throw new NumberInStringException();
+		}
+	}
+
+	public static void NumberOutOfBoundsException(int num) throws NumberOutOfBoundException {
+		if (num >= 20 || num == 0) {
+			throw new NumberOutOfBoundException();
+		}
+		}
+
+	
+//	public static void InputMismatchException () throws InputMismatchException{
+//		throw new InputMismatchException();
+//	}
+
+}
