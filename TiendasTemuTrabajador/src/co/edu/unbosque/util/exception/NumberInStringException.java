7a84904e1722c629378172934fac8df6f710b241
Gomsss16 <jugovi1609@gmail.com>
2025-03-30 16:16:44 -0500

    Delete TiendasTemuTrabajador/src/co/edu/unbosque/util/exception/NumberInStringException.java

--- TiendasTemuTrabajador/src/co/edu/unbosque/util/exception/NumberInStringException.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unbosque.util.exception;
-
-public class NumberInStringException extends Exception {
-
-	public NumberInStringException() {
-
-	super ("Aca solo van letras, intente denuevo mas tarde");
-	}
-}
